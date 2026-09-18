44ff81220dedf2799f324ea118f063ae27d44780
adisburk <adisblnu13@gmail.com>
2026-09-18 13:58:57 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,64 @@
 
 using namespace geode::prelude;
 
+class MyCustomPopup : public FLAlertLayer {
+public:
+    static MyCustomPopup* create() {
+        auto ret = new MyCustomPopup();
+        if (ret && ret->init()) {
+            ret->autorelease();
+            return ret;
+        }
+        CC_SAFE_DELETE(ret);
+        return nullptr;
+    }
+
+    bool init() {
+        if (!FLAlertLayer::init(150)) return false;
+
+        auto background = CCScale9Sprite::create("GJ_square01.png");
+        background->setContentSize({ 260, 200 });
+        
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
+        background->setPosition(winSize / 2);
+        m_mainLayer->addChild(background);
+
+        auto label = CCLabelBMFont::create("OLO", "bigFont.fnt");
+        label->setScale(0.7f);
+        label->setPosition(winSize / 2 + CCPoint{0, 65});
+        m_mainLayer->addChild(label);
+
+        auto photo = CCSprite::create("my_photo.png"_spr);
+        if (photo) {
+            photo->getTexture()->setAliasTexParameters();
+            photo->setScale(60.0f / photo->getContentSize().width);
+            photo->setPosition(winSize / 2 + CCPoint{0, 5});
+            m_mainLayer->addChild(photo);
+        }
+
+        auto okButtonSprite = ButtonSprite::create("OK");
+        auto okButton = CCMenuItemSpriteExtra::create(
+            okButtonSprite,
+            this,
+            menu_selector(MyCustomPopup::onClose)
+        );
+
+        auto menu = CCMenu::create();
+        menu->addChild(okButton);
+        menu->setPosition(winSize / 2 + CCPoint{0, -60});
+        m_mainLayer->addChild(menu);
+
+        this->setKeypadEnabled(true);
+        this->setTouchEnabled(true);
+
+        return true;
+    }
+
+    void onClose(CCObject* sender) {
+        this->keyBackClicked();
+    }
+};
+
 class $modify(MyMenuLayer, MenuLayer) {
     bool init() {
         if (!MenuLayer::init()) return false;
@@ -26,21 +84,9 @@ class $modify(MyMenuLayer, MenuLayer) {
     }
 
     void onLolButtonClick(CCObject* sender) {
-        auto alert = FLAlertLayer::create("OLO", " ", "OK");
-        alert->show();
-
-        auto mainLayer = alert->m_mainLayer;
-        if (mainLayer) {
-            auto photo = CCSprite::create("my_photo.png"_spr);
-            if (photo) {
-                photo->getTexture()->setAliasTexParameters();
-                photo->setScale(95.0f / photo->getContentSize().width);
-                
-                auto winSize = CCDirector::sharedDirector()->getWinSize();
-                photo->setPosition(winSize / 2 + CCPoint{0, 15});
-                
-                mainLayer->addChild(photo, 100);
-            }
+        auto popup = MyCustomPopup::create();
+        if (popup) {
+            popup->show();
         }
     }
 };
